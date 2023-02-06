7ab67f9bc4bbb3305d0e4e5ce305386e1960a254
dodam24 <121652059+dodam24@users.noreply.github.com>
2023-02-06 23:48:23 +0900

    Update AddGame.java

--- 03_loop/src/for_/AddGame.java
+++ 03_loop/src/for_/AddGame.java
@@ -9,22 +9,27 @@ public class AddGame {
 		
 		int a, b, dab, count=0;
 		
-		//5문제 제공
+		// 5문제 제공
 		for(int i=1; i<=5; i++) {
-			a = (int)(Math.random() * (99-10+1) + 10);		//10 ~ 99
+			a = (int)(Math.random() * (99-10+1) + 10);		// 10 ~ 99
 			b = (int)(Math.random() * 90 + 10);
 			
-			System.out.print("[" + i + "] " + a + " + " + b + " = ");
-			dab = scan.nextInt();
+			for(int j=1; j<=2; j++) {
+				System.out.print("[" + i + "] " + a + " + " + b + " = ");
+				dab = scan.nextInt();
 			
-			if(dab == a+b) {
-				System.out.println("참 잘했어요"); 
-				count++;
-			}
-			else System.out.println("틀렸다");
+				if(dab == a+b) {
+					System.out.println("참 잘했어요"); 
+					count++;
+					break; 	// for j를 벗어나기
+				}
+				else 
+					if(j == 1) System.out.println("틀렸다");
+					else if(j == 2) System.out.println("틀렸다. 정답은 " + (a+b));
+			}	// 틀리면 한 번 더 기회 주기 (틀렸을 때, 난수가 또 발생하면 안 됨)
 		}
 		System.out.println();
-		System.out.println("당신은 총 " + count + "문제를 맞추어서 점수 " + count*20 + "점 입니다.");
+		System.out.println("당신은 총 " + count + "문제를 맞추어서 점수 " + count * 20 + "점 입니다.");
 	
 	}
 
@@ -37,7 +42,7 @@ public class AddGame {
 - 총 5문제를 제공한다 (for문)
 - 1문제당 20점씩 처리한다.
 
-[실행결과] 	//틀리면 한 번 더 기회 주기 (for, if, break 사용)
+[실행결과] 		// 틀리면 한 번 더 기회 주기 (for, if, break 사용)
 	a		b
 [1] 87 + 56 = 78
 틀렸다
@@ -50,5 +55,5 @@ public class AddGame {
 [5] 82 + 45 = 78
 틀렸다
 
-당신은 총 x문제를 맞추어서 점수 xx(count변수 *20)점 입니다.
+당신은 총 x문제를 맞추어서 점수 xx(count변수 * 20)점 입니다.
 */
