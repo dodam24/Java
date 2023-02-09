cd75bc600c190eb664140e24163ca74ceaf1ee2f
dodam24 <dodam0724@gmail.com>
2023-02-09 23:41:15 +0900

    2023-02-08 homework

--- 04_array/src/multi/MultiArray05.java
+++ 04_array/src/multi/MultiArray05.java
@@ -5,43 +5,69 @@ import java.util.Scanner;
 public class MultiArray05 {
 
 	public static void main(String[] args) {
-		//name, subjectCnt, subject, jumsu 선언
-		String name, subject;
-		int subjectCnt, jumsu;
-		
-		int[][] ar = new int[2][];
-		ar[0] = new int[5];
-		ar[1] = new int[6];
-		
 		Scanner scan = new Scanner(System.in);
-		System.out.println("인원수 : ");
-		int cnt = scan.nextInt(); 
 		
+		System.out.print("인원수: ");
+		int cnt = scan.nextInt();
 		
-		for(int i=0; i<cnt; i++) {
-			System.out.print("이름입력 : ");
-				name = scan.next();
-			System.out.print("과목수 입력 : ");
-				subjectCnt = scan.nextInt();
+		String[] name = new String[cnt];
+		int subjectCnt;
+		String subject[][] = new String[cnt][];
+		int jumsu[][] = new int[cnt][];
+		double[] avg = new double[cnt];
 		
+		for(int i=0; i<cnt; i++) { //인원수 
+			System.out.print("이름 입력: ");
+			name[i] = scan.next();
+			
+			System.out.print("과목수 입력: ");
+			subjectCnt = scan.nextInt();
+			
+			subject[i] = new String[subjectCnt]; //열 생성
+			for(int j=0; i<subjectCnt; j++) {
+			System.out.print("과목명 입력: ");
+			subject[i][j] = scan.next();
+			} //for j
+			
+			jumsu[i] = new int[subjectCnt+1];
 			for(int j=0; j<subjectCnt; j++) {
-				System.out.print("과목명 입력 : ");
-					subject = scan.next();
-				System.out.print("점수 입력 : ");
-					jumsu = scan.nextInt();
+				System.out.print(subject[i][j] + " 점수 입력: ");
+				jumsu[i][j] = scan.nextInt();
 				
-			}
+				//총점
+				jumsu[i][subjectCnt] += jumsu[i][j];
+			} //for j
 			
-			
+			//평균
+			avg[i] = (double)jumsu[i][subjectCnt] / subjectCnt;
 		}
+		System.out.println("------------------------------------");
+		// for i
 		
+		//출력 
+		for(int i=0; i<cnt; i++) { //인원수
+			
+			//타이틀
+			System.out.print("이름\t");
+			for(int j=0; j<subject[i].length; j++) {
+				System.out.print(subject[i][j] + "\t");
+			} //for j 
+			System.out.print("총점\t평균");
+			
+			//데이터
+			System.out.print("\n" + name[i] + "\t");
+			for(int j=0; j<jumsu[i].length; j++) { //과목, 총점
+				System.out.print(jumsu[i][j] + "\t");	
+			} //for j
+			System.out.println(String.format("%.2f", avg[i]) + "\n");
+		} //for i
 		
-		
-		
-		
-
 	}
+	
 }
+	
+
+
 
 /* 
 [문제] //과목수가 다르므로 가변 길이 사용
@@ -73,5 +99,78 @@ public class MultiArray05 {
 
 이름     국어  	영어   과학    총점      평균
 이기자    95   	100    90    xxx      xx.xx
+
+*/
+
+
+/* 
+package multi;
+
+import java.util.Scanner;
+
+public class MultiArray05 {
+
+   public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      
+      System.out.print("인원수: ");
+      int cnt = scan.nextInt();
+      
+      String[] name = new String[cnt];
+      int subjectCnt;
+      String subject[][] = new String[cnt][];
+      int jumsu[][] = new int[cnt][];
+      double[] avg = new double[cnt]; 
+      
+      for(int i=0; i<cnt; i++) { //인원수
+         System.out.print("이름 입력: ");
+           name[i] = scan.next();
+           
+           System.out.print("과목수 입력: ");
+           subjectCnt = scan.nextInt();
+           
+           subject[i] = new String[subjectCnt]; //열 생성
+           for(int j=0; j<subjectCnt; j++) {
+              System.out.print("과목명 입력: ");
+              subject[i][j] = scan.next();
+           }//for j
+           
+           jumsu[i] = new int[subjectCnt+1];
+           for(int j=0; j<subjectCnt; j++) {
+              System.out.print(subject[i][j] + " 점수 입력: ");
+              jumsu[i][j] = scan.nextInt();
+              
+              //총점
+              jumsu[i][subjectCnt] += jumsu[i][j];
+           }//for j
+           
+           //평균
+           avg[i] = (double)jumsu[i][subjectCnt] / subjectCnt;
+           
+           System.out.println("---------------------");
+      }//for i
+      
+      //출력
+      for(int i=0; i<cnt; i++) { //인원수
+         
+         //타이틀
+         System.out.print("이름\t");
+         for(int j=0; j<subject[i].length; j++) {
+            System.out.print(subject[i][j] + "\t");
+         }//for j
+         System.out.print("총점\t평균");
+         
+         //데이터
+         System.out.print("\n" + name[i] + "\t");
+         for(int j=0; j<jumsu[i].length; j++) { //과목, 총점
+            System.out.print(jumsu[i][j] + "\t");
+         }//for j
+         System.out.println(String.format("%.2f", avg[i]) + "\n");
+         
+      }//for i
+   
+   }
+
+}
 
 */
